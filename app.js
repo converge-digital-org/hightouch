58f1a0c77eac659ed5dd74c005276325bff0d3bd
converge-digital-org <kyle@converge.digital>
2024-11-26 15:24:52 -0800

    Update app.js

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
-// HIGHTOUCH EVENTS APP.JS FILE –– LAST UPDATED: 11/26/2024 AT 3:14 PM PT //
-// Additions: Implemeted 'add_to_cart', 'remove_from_cart', and 'view_cart' events
+// HIGHTOUCH EVENTS APP.JS FILE –– LAST UPDATED: 11/26/2024 AT 3:24 PM PT //
+// Additions: Implemeted 'add_to_cart', 'remove_from_cart', 'view_cart', 'begin_checkout' events
 
 console.log("Hightouch Events script loaded");
 
